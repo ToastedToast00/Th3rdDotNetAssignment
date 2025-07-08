5214172969423b4c83cdc8e96abb77621fd797ed
Toasty <toastedtoast0090@gmail.com>
2025-07-07 17:07:46 -0700

    implemented GameSelect and added methods for intended games to play

--- Program.cs
+++ Program.cs
@@ -1,11 +1,67 @@
-﻿public class Program
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+public class Program
 {
     public static void Main(string[] args)
     {
         GameSelect();
     }
-    public static void GameSelect() 
-    { 
-        
+    public static void Instructions() 
+    {
+        Console.WriteLine("This Program allows the user to play a card game");
+        Console.WriteLine("Enter 5 to stop");
+        Console.WriteLine("please select a game to play");
+        Console.WriteLine();
+        Console.WriteLine("  1. Black Jack");
+        Console.WriteLine("  2. Poker");
+        Console.WriteLine("  3. Go Fish");
+        Console.WriteLine("  4. War");
+    }
+    public static void GameSelect()
+    {
+        Instructions();
+        string? input = Console.ReadLine();
+        switch (input) 
+        {
+            case "1":
+                BlackJack();
+                break;
+            case "2":
+                Poker();
+                break;
+            case "3":
+                GoFish();
+                break;
+            case "4":
+                War();
+                break;
+            default:
+                Console.WriteLine("Invalid selection, please try again.");
+                GameSelect();
+                break;
+        }
+    }
+    public static void CardGenerator()
+    {
+        //TODO: Implement card generator logic here
+        //      used by 1 game in GameSelect
+    }
+    public static void BlackJack()
+    {
+        //TODO: using CardGenerator() play a game of blackjack
+    }
+    public static void Poker()
+    {
+        //TODO: using CardGenerator() play a game of poker
+    }
+    public static void GoFish() 
+    {
+        //TODO: using CardGenerator() play a game of Go Fish
+    }
+    public static void War() 
+    {
+        //TODO: using CardGenerator() play a game of War
     }
 }
